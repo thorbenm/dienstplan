7ea30839cab1f0d55c2a53adb71b61b7b894db4f
Thorben Muehleder <Thorben@Thorbens-MacBook-Pro.local>
2017-05-02 16:47:15 +0200

    constructor and first object of hebamme

--- main.cpp
+++ main.cpp
@@ -3,7 +3,13 @@
 struct hebamme{
 	std::string name;
 	char kuerzel;
+		hebamme(std::string name1, char kuerzel1){
+		name = name1;
+		kuerzel = kuerzel1;
+		}
 };
+
+hebamme heb1("heb1", '1');
 
 int main(){
 	std::cout << "hello world";
