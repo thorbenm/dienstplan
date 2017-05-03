bfb9c9a87b4beff78529c5237d0e0c895cd51b84
Thorben Muehleder <Thorben@Thorbens-MacBook-Pro.local>
2017-05-03 09:53:05 +0200

    random number generator now works

--- main.cpp
+++ main.cpp
@@ -1,19 +1,26 @@
 #include <iostream>
 #include <string>
-#include <random>
+//#include <random>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
-using namespace std;
-
-const int noh = 5;
+const int noh = 3;
 
 struct hebamme{
-	string name;
+	std::string name;
 	char kuerzel;
 } heb[noh];
 
 
 int main(){
-
-	std::cout << random() << std::endl;
+	srand (time(NULL));
+	heb[0].name = "null";	
+	heb[0].kuerzel = '0';
+	heb[1].name = "eins";	
+	heb[1].kuerzel = '1';
+	heb[2].name = "zwei";	
+	heb[2].kuerzel = '2';
+	std::cout << rand() % noh << std::endl;
 	return 0;
 }
