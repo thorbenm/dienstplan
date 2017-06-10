9bb3a9c7d775239737474be86bfeeaf510b7ccde
Thorben B. Muehleder <thorbenm@me.com>
2017-06-10 19:27:29 +0200

    nice welcome message

--- dienstplan_erstellen.sh
+++ dienstplan_erstellen.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 BASEDIR=$(dirname "$0")
 cd "$BASEDIR"
+rm main.cpp
 cat sourcecode/main.cpp1 >> main.cpp
-cat freiwuensche.txt >> main.cpp
+cat info.txt >> main.cpp
 cat sourcecode/main.cpp2 >> main.cpp
 g++ -std=c++11 main.cpp
 ./a.out
