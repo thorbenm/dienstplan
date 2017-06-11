96e0a1d29062828cf316da2e15e7c413340a1a59
Thorben B. Muehleder <thorbenm@me.com>
2017-06-11 15:25:01 +0200

    textedit messed up the quotation marks, so now nano is used

---
+++ dienstplan.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+BASEDIR=$(dirname "$0")
+cd "$BASEDIR"
+rm main.cpp
+nano .sourcecode/info.txt
+cat .sourcecode/main.cpp1 >> main.cpp
+cat .sourcecode/info.txt >> main.cpp
+cat .sourcecode/main.cpp2 >> main.cpp
+g++ -std=c++11 main.cpp
+./a.out
+rm a.out
+rm main.cpp
